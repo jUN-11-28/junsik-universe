376c422519bbee9cb07c670ef43c856f136adf67
jUN-11-28 <101115031+jUN-11-28@users.noreply.github.com>
2022-09-30 22:28:35 +1000

    recirect to craft file

--- getMusicInfo.py
+++ getMusicInfo.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
-from YouTubeMusicAPI import YouTubeMusicAPI
+#from YouTubeMusicAPI import YouTubeMusicAPI
 
 # webdriver을 통해 Chrome 웹 드라이버 객체를 만들어 줌
 # browser = webdriver.Chrome()
 
 # Chrome 웹 드라이버 객체에게 get을 통하여 네이버의 HTTP 요청을 하게 만듬
-# browser.get("https://music.youtube.com/browse/VLOLAK5uy_mgryJlILDE846ev_oLUtqPgtKuA92zdBs")
+ browser.get("https://music.youtube.com/browse/VLOLAK5uy_mgryJlILDE846ev_oLUtqPgtKuA92zdBs")
 
-track = YouTubeMusicAPI("marshmello").track()
-print(track)
+#track = YouTubeMusicAPI("marshmello").track()
+#print(track)
