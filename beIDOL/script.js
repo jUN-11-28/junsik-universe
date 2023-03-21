dd01de44945a16638826d6f2c5a1dd3bfc16bf19
jUN-11-28 <101115031+jUN-11-28@users.noreply.github.com>
2023-03-21 21:34:41 +1100

    backgorud 계산

--- beIDOL/script.js
+++ beIDOL/script.js
@@ -9,38 +9,42 @@ function sendUserInput() {
   if (userInput === '') {
     return;
   }
+	addUserBubble(userInput);
+  document.querySelector("#input-text").focus();
+}
 
-  // 현재 시간을 구합니다.
-  const currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+function addUserBubble(userInput) {
+	// 현재 시간을 구합니다.
+	const currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 
-  // 메시지를 추가할 요소를 생성합니다.
+	// 메시지를 추가할 요소를 생성합니다.
 	const messageText = document.createElement("pre");
-  const messageTime = document.createElement("p");
-  const messageContainer = document.createElement("div");
-  const messageBubble = document.createElement("div");
+	const messageTime = document.createElement("p");
+	const messageContainer = document.createElement("div");
+	const messageBubble = document.createElement("div");
 
-  // 요소에 클래스를 추가합니다.
-  messageTime.classList.add("message-time");
-  messageContainer.classList.add("message-container");
-  messageBubble.classList.add("user-message-bubble");
-  messageText.classList.add("message-text");
+	// 요소에 클래스를 추가합니다.
+	messageTime.classList.add("message-time");
+	messageContainer.classList.add("message-container");
+	messageBubble.classList.add("user-message-bubble");
+	messageText.classList.add("message-text");
 
-  // 요소에 사용자가 입력한 메시지와 현재 시간을 추가합니다.
-  messageText.textContent = userInput;
-  messageTime.textContent = currentTime;
+	// 요소에 사용자가 입력한 메시지와 현재 시간을 추가합니다.
+	messageText.textContent = userInput;
+	messageTime.textContent = currentTime;
 
-  // 요소를 채팅창에 추가합니다.
+	// 요소를 채팅창에 추가합니다.
 	messageContainer.appendChild(messageTime);
-  messageContainer.appendChild(messageBubble);
-  messageBubble.appendChild(messageText);
-  chatHistory.appendChild(messageContainer);
+	messageContainer.appendChild(messageBubble);
+	messageBubble.appendChild(messageText);
+	chatHistory.appendChild(messageContainer);
 
-  // 입력창을 초기화합니다.
-  document.querySelector("#user-input").value = "";
+	// 입력창을 초기화합니다.
+	document.querySelector("#user-input").value = "";
 
-  // 입력창의 높이를 자동으로 조절합니다.
-  adjustUserInputHeight();
-  chatHistory.scrollTop = chatHistory.scrollHeight;
+	// 입력창의 높이를 자동으로 조절합니다.
+	adjustUserInputHeight();
+	chatHistory.scrollTop = chatHistory.scrollHeight;
 }
 
 // 입력창의 높이를 자동으로 조절하는 함수입니다.
@@ -63,6 +67,16 @@ function handleKeyPress(event) {
   }
 }
 
+const userInputContainer = document.querySelector("#user-input-container");
+const background = document.querySelector("#background");
+
+function adjustBackgroundHeight() {
+  const userInputContainerHeight = userInputContainer.offsetHeight;
+  const windowHeight = window.innerHeight;
+  background.style.height = `${windowHeight - userInputContainerHeight - 15}px`;
+	
+}
+
 // 초기화 함수입니다.
 function init() {
   // 입력창의 높이를 자동으로 조절합니다.
@@ -79,6 +93,10 @@ function init() {
       userInput.value += "\n"; // 대신 개행 추가
     }
   });
+
+	// 백그라운드 크기 조절
+	adjustBackgroundHeight();
+  window.addEventListener("resize", adjustBackgroundHeight);
 }
 
 // 초기화 함수를 호출합니다.
